2c6426c5f28386e9ee69b0d40fa7bcfbe8c5e485
yehonatan <yoni333@gmail.com>
2017-10-31 20:08:17 +0200

    first 6 vue example comapre to same function in JS es15 an jquery 2.2.4

---
+++ script.js
@@ -0,0 +1,140 @@
+// first example 
+app= new Vue({
+    el:'#app',
+    data:{message:"Vue here"}
+});
+
+//vanila
+var vanil = document.getElementById("vanilla");
+vanil.innerHTML = "vanilla here";
+//jquery
+var jq = $("#jquery");
+jq.text("jquery here");
+
+//example 2 attribute bind
+
+
+app= new Vue({
+    el:'#app2',
+    data:{message:'You loaded this page on ' + new Date().toLocaleString()}
+});
+
+//vanila
+vanil = document.getElementById("vanilla2");
+vanil.children[0].title = "vanila title here"
+vanil.querySelector('span').title= vanil.children[0].title + " with query selector";
+//jquery
+jq = $("#jquery2 > span");
+
+jq.prop("title","jquery title here");
+
+
+//example 3  attribute bind
+
+
+app3= new Vue({
+    el:'#app3',
+    data:{seen:true}
+});
+
+//vanila
+vanil = document.getElementById("vanilla3");
+vanil.hidden=false;
+//jquery
+jq = $("#jquery3");
+
+jq.prop("hidden",false);
+
+//example 4 loops
+var app4 = new Vue({
+    el: '#app4',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  })
+
+  
+//vanila
+var todos= [
+    { text: 'Learn JavaScript' },
+    { text: 'Learn Vue' },
+    { text: 'Build something awesome' }
+  ];
+
+vanil = document.getElementById("vanilla4");
+todos.forEach(function (obj){
+    var node = document.createElement("LI");                 // Create a <li> node
+    //var textnode = document.createTextNode(obj.text);         // Create a text node
+    //node.appendChild(textnode);                              // Append the text to <li>
+    node.innerText=obj.text;                              // Append the text to <li>
+   vanil.querySelector("ol").appendChild(node);     // Append <li> to <ol> 
+});
+
+//jquery
+jq = $("#jquery4 > ol ");
+$.each(todos,function(index,obj){
+
+    jq.append("<li>"+obj.text+"</li>");
+});
+
+//example 5 one direction event binding
+
+var app5 = new Vue({
+    el:"#app5",
+    data:{
+        message:"reverse text example"
+    },
+    methods:{
+        reverseMessage:function(){
+            this.message = this.message.split('').reverse().join('');
+        }
+        
+    }
+})
+
+//vanilla
+// vanil5 = document.getElementById("vanilla5");
+// v5p = vanil5.querySelector("p");
+// v5p.innerText = "vanilla reverse";
+// function reverseMessage(){
+
+//     v5p.innerText =  v5p.innerText.split('').reverse().join('');
+
+// }
+
+function vanilla_OBJ( element,text) {
+
+    this.vanil5 =  document.getElementById(element);
+    this.v5p = this.vanil5.querySelector("p");
+    this.text= this.v5p.innerText = text;
+    this.click=function reverseMessage(){
+    
+        this.v5p.innerText =  this.v5p.innerText.split('').reverse().join('');
+    
+    }
+
+
+}
+vanilla5 = new vanilla_OBJ ( "vanilla5" , "vanilla reverse");
+
+//jquery
+jq5paragraph = $('#jquery5 p');
+jq5paragraph.text("jquery reverse");
+jq5button = $('#jquery5 button');
+jq5button.click(function (){
+    jq5paragraph.text( jq5paragraph.text().split('').reverse().join('') ) ;
+});
+
+
+// example 6 two direction event binding
+var  app6= new Vue(
+       {el:"#app6",
+       data:{
+           message:"hello vue meetup",
+           message2:"second 2 direction binding"
+        }
+    });
